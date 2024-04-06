4b00dd4c6dfdf553c037eba1dbdbd7c5e5bf749d
Maria Kuvshinova <mkuvshinova96@gmail.com>
2024-04-06 21:25:40 +0500

    Add Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 2: Напишите программу вычисления функции
+// Аккермана с помощью рекурсии. Даны два
+// неотрицательных числа m и n. 
+
+// m = 2, n = 3 -> A(m,n) = 29   !!! В примере ошибка, согласно википедии переменные перепутаны,
+//                                   т.е должно быть A(n,m), например: 
+//                                   А(2, 3) = 9, а вот А(3, 2) = 29
+// m = 11, n = 3-> A(m,n) = 16381   !!! Здесь также А(3, 11) = 16381
+
+int AckermannFunction(int n, int m)
+{
+    if (n == 0)
+    {
+        return m + 1;
+    }
+
+    else if (m == 0)
+    {
+        return AckermannFunction(n - 1, 1);
+    }
+
+    else
+    {
+        return AckermannFunction(n - 1, AckermannFunction(n, m - 1));
+    }
+}
+
+Console.Write(AckermannFunction(3, 11));
